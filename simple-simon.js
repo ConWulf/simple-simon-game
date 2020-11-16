b8c5d5b591127720bdc79189e167fa773f13ba7b
Connor Wulf <connor.m.wulf@gmail.com>
2020-11-15 18:46:34 -0600

    changed flash animation to use toggle and interval instead if css animation

--- simple-simon.js
+++ simple-simon.js
@@ -1,36 +1,18 @@
 (function () {
     "use strict"
 
-    let red = Math.floor(Math.random() * 256);
-    let green = Math.floor(Math.random() * 256);
-    let blue = Math.floor(Math.random() * 256);
 
-    let leftSection = document.getElementById("topLeft")
-
-function backgroundFade (r, g, b) {
-    leftSection.style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")"
-    leftSection.style.transition =  "background-color 1s";
+    let leftSection = $('#topLeft');
 
 
+    function backgroundFade () {
+        leftSection.toggleClass('opacity');
 }
 
+let flash = setInterval(backgroundFade, 1000)
 
 
-
-// leftSection.onclick = function () {
-//     leftSection.style.backgroundColor = "rgb(" + red + "," + green + "," + blue + ")"
-// }
-
-
-
-
-
-
-
-
-
-
-
+backgroundFade();
 
 
 })();
